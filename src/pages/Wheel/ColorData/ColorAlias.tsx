6ef49aebe99c7fc2df8299217f47c972f98f43e6
Denys Grybov <denys@grybov.com>
2020-09-02 11:26:05 -0500

    Update Details section
    Add new features
    - Copy a color scheme
    - Switch an output format: JSON/SASS
    - Allow customization of color keys

---
+++ src/pages/Wheel/ColorData/ColorAlias.tsx
@@ -0,0 +1,92 @@
+import React, { useCallback, useState } from 'react';
+import {
+  Box,
+  Button,
+  Grid,
+  Flex,
+  Input,
+  Text,
+} from '@chakra-ui/core';
+
+import { useForm } from 'react-hook-form';
+
+import { orderedColors } from './constants';
+import { ColorAlias as ColorAliasData, ColorAliasProps } from './colorData.d';
+
+const ColorAlias: React.FC<ColorAliasProps> = ({
+  value: schema,
+  onChange,
+}) => {
+  const [ isColorAliasVisible, setisColorAliasVisible ] = useState<boolean>(false);
+  const handleAliasExpand = useCallback(() => setisColorAliasVisible(true), []);
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = (data: ColorAliasData) => {
+    const normalizedData = Object
+      .keys(data)
+      .filter(key => data[key])
+      .reduce((result, key) => ({
+        ...result,
+        [key.replace('color_alias_', '')]: data[key],
+      }), {});
+
+    onChange(normalizedData);
+  };
+
+  return isColorAliasVisible ? (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Grid
+        marginTop="4"
+        templateColumns={[
+          '2.5rem 1fr',
+          '2.5rem 1fr',
+          '2.5rem 1fr 2.5rem 1fr',
+          '2.5rem 1fr 2.5rem 1fr 2.5rem 1fr',
+        ]}
+        gap="4"
+        marginBottom="4"
+      >
+        {orderedColors.map(color => (
+          <React.Fragment key={color}>
+            <Box
+              width="10"
+              height="10"
+              backgroundColor={schema?.[color]?.[500]}
+              rounded="md"
+            />
+            <Box>
+              <Input
+                flexGrow={1}
+                name={`color_alias_${color}`}
+                ref={register({ validate: (value) => /^[A-Za-z_-]*$/.test(value) || 'Incorrect value. Only letters, _ and - allowed' })}
+                placeholder={color.toLocaleLowerCase()}
+                backgroundColor="gray.700"
+                isInvalid={errors[`color_alias_${color}`]}
+              />
+              {errors[`color_alias_${color}`] && <Text fontSize="xs">{errors[`color_alias_${color}`].message}</Text>}
+            </Box>
+          </React.Fragment>
+        ))}
+        <div></div>
+        <Button variantColor="purple" type="submit">Update color names</Button>
+      </Grid>
+    </form>
+  ) : (
+    <Flex
+      marginTop="4"
+      flexDirection="column"
+      justifyContent="center"
+    >
+      <Button
+        margin="0 auto"
+        variantColor="blue"
+        onClick={handleAliasExpand}
+      >
+        Set custom color names
+      </Button>
+    </Flex>
+  );
+};
+
+ColorAlias.displayName = 'ColorAlias';
+
+export default ColorAlias;
---
+++ src/pages/Wheel/ColorData/constants.ts
@@ -0,0 +1,39 @@
+import { ColorAlias } from './colorData.d';
+
+export const orderedColors = [
+  'RED',
+  'RED_VIOLET',
+  'VIOLET',
+  'BLUE_VIOLET',
+  'BLUE',
+  'BLUE_GREEN',
+  'GREEN',
+  'YELLOW_GREEN',
+  'YELLOW',
+  'YELLOW_ORANGE',
+  'ORANGE',
+  'RED_ORANGE',
+  'GRAY',
+  'BLACK',
+  'WHITE',
+];
+
+export const shadesList = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+
+export const defaultColorAlias: ColorAlias = {
+  RED: 'red',
+  RED_VIOLET: 'red_violet',
+  VIOLET: 'violet',
+  BLUE_VIOLET: 'blue_violet',
+  BLUE: 'blue',
+  BLUE_GREEN: 'blue_green',
+  GREEN: 'green',
+  YELLOW_GREEN: 'yellow_green',
+  YELLOW: 'yellow',
+  YELLOW_ORANGE: 'yellow_orange',
+  ORANGE: 'orange',
+  RED_ORANGE: 'red_orange',
+  GRAY: 'gray',
+  BLACK: 'black',
+  WHITE: 'white',
+};
