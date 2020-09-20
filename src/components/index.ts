01eaa02ac6ef92ece18f4f43943dc8bc38fc5903
Denys Grybov <denys@grybov.com>
2020-09-20 15:10:44 -0500

    Add PagePlaceholder for page lazy loading

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,5 +4,6 @@ export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 export { default as Layout } from './Layout';
 export { default as Page } from './Page';
+export { default as PagePlaceholder } from './PagePlaceholder';
 export { default as Pagination } from './Pagination';
 export { default as Range } from './Range';
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,11 @@
 import React, { Suspense, useMemo, useState } from 'react';
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 import { Global } from '@emotion/core';
-import {
-  BrowserRouter,
-  Route,
-  Switch,
-} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Layout from 'components/Layout';
 import PageDataContext from 'components/Page/PageContext';
+import PagePlaceholder from 'components/PagePlaceholder';
 import customTheme from 'services/theme';
 import menuJson from 'services/menu-items.json';
 import globalStyles from 'styled/global';
@@ -20,9 +17,7 @@ const AboutPage = React.lazy(() => import('pages/About'));
 const WheelPage = React.lazy(() => import('pages/Wheel'));
 const Page404 = React.lazy(() => import('pages/Page404'));
 
-interface IApp {}
-
-const App: React.FC<IApp> = () => {
+const App: React.FC = () => {
   const [title, setTitle] = useState('');
 
   const pageContextState = useMemo(() => ({ title, setTitle }), [title, setTitle]);
@@ -33,7 +28,7 @@ const App: React.FC<IApp> = () => {
       <BrowserRouter>
         <PageDataContext.Provider value={pageContextState}>
             <Layout menuItems={menuItems}>
-              <Suspense fallback={<div>Loading...</div>}>
+              <Suspense fallback={PagePlaceholder}>
                 <Switch>
                   <Route path="/about" exact component={AboutPage} />
                   <Route path="/" exact component={WheelPage} />
@@ -46,5 +41,7 @@ const App: React.FC<IApp> = () => {
     </ThemeProvider>
   );
 };
+
+App.displayName = 'Veselka';
 
 export default App;
