4a1a7687497afac80204938530a03fb0474f9730
Denys Grybov <denys@grybov.com>
2020-10-16 14:07:10 -0500

    Add favicon update link preview

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -11,6 +11,10 @@ import menuJson from 'services/menu-items.json';
 import globalStyles from 'styled/global';
 import { MenuItem } from 'types';
 import UrlContext, { useUrlContext } from 'services/UrlContext';
+import { Helmet } from 'react-helmet';
+
+import favicon from './favicon.svg';
+import { useTranslation } from 'react-i18next';
 
 const menuItems = menuJson as unknown as MenuItem[];
 
@@ -20,12 +24,32 @@ const Page404 = React.lazy(() => import('pages/Page404'));
 
 const App: React.FC = () => {
   const [title, setTitle] = useState('');
+  const { t } = useTranslation('general');
 
   const pageContextState = useMemo(() => ({ title, setTitle }), [title, setTitle]);
   const urlContextvalue = useUrlContext();
 
   return (
     <ThemeProvider theme={customTheme}>
+      <Helmet>
+        <link
+          rel="icon"
+          type="image/png"
+          href={favicon}
+        />
+        <meta
+          property="og:title"
+          content={t('title')}
+        />
+        <meta
+          property="og:image"
+          content={favicon}
+        />
+        <meta
+          property="og:description"
+          content={t('descriptoin')}
+        />
+      </Helmet>
       <UrlContext.Provider value={urlContextvalue}>
       <CSSReset />
       <Global styles={globalStyles} />
