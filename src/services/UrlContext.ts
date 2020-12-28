ddcaf2a56323952ff7a2e70b3d17dc6f36d452cb
Denys Grybov <denys@grybov.com>
2020-12-27 20:53:17 -0600

    Update wheel page
    + remove ColorData component
    * update typing for urlcontext

--- src/services/UrlContext.ts
+++ src/services/UrlContext.ts
@@ -1,28 +1,27 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useLink } from 'hooks';
 import { UrlProps } from 'pages/Wheel/types';
-import { orderedColors, defaultColorAlias } from 'pages/Wheel/ColorData/constants';
-import { ColorAliasList } from 'pages/Wheel/ColorData/ColorAlias/colorAlias.d';
-
+import { orderedColors, defaultColorAlias } from 'services/constants';
+import { ColorAlias } from 'types';
 interface UpdateUrlProps {
-  colorAlias?: ColorAliasList;
+  colorAlias?: ColorAlias;
   shade?: string;
   balance?: number;
 }
 
 interface ContextValue {
-  colorAlias: ColorAliasList;
+  colorAlias: ColorAlias;
   shade: string;
   balance: number;
   updateUrl: (update: UpdateUrlProps) => void;
 }
 
-const DEFAULT_COLOR_ALIAS = {};
+const DEFAULT_COLOR_ALIAS: ColorAlias = {...defaultColorAlias};
 const DEFAULT_SHADE = '7f7f7f';
 const DEAFULT_BALANCE = 0.3;
 
 export const useUrlContext = () => {
-  const [colorAlias, setColorAlias] = useState<ColorAliasList>(DEFAULT_COLOR_ALIAS);
+  const [colorAlias, setColorAlias] = useState<ColorAlias>(DEFAULT_COLOR_ALIAS);
   const [shade, setShade] = useState<string>(DEFAULT_SHADE);
   const [balance, setBalance] = useState<number>(DEAFULT_BALANCE);
   const {queryParams, updateURL} = useLink<UrlProps>();
@@ -44,17 +43,19 @@ export const useUrlContext = () => {
 
     if (shade) {
       updateObject.c = shade;
+      setShade(shade);
     }
 
     if (balance) {
       updateObject.w = balance;
+      setBalance(balance);
     }
 
     updateURL(updateObject);
   }, [updateURL]);
 
   useEffect(() => {
-    let aliasValue: ColorAliasList = DEFAULT_COLOR_ALIAS;
+    let aliasValue: ColorAlias = {...DEFAULT_COLOR_ALIAS};
 
     if (queryParams?.a) {
       const urlAlias = queryParams.a.split('~');
@@ -80,7 +81,7 @@ export const useUrlContext = () => {
 };
 
 const UrlContext = React.createContext<ContextValue>({
-  colorAlias: {},
+  colorAlias: defaultColorAlias,
   shade: DEFAULT_SHADE,
   balance: DEAFULT_BALANCE,
   updateUrl: (update: UpdateUrlProps) => {},
