1f625070a036a521cfab716e66e21d0223f1f4ba
Denys Grybov <denys@grybov.com>
2020-12-27 20:52:05 -0600

    Move Spectre component to organisms

--- src/organisms/index.ts
+++ src/organisms/index.ts
@@ -1,3 +1,4 @@
 export { default as Circle } from './Circle';
 export { default as ColorAlias } from './ColorAlias';
 export { default as SchemeOutput } from './SchemeOutput';
+export { default as Spectre } from './Spectre';
